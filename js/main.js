072d1a6e588c4edca571d8ef46c7a9fa2e6aa569
Alexander Kelly <alex.r.kelly492@gmail.com>
2017-05-20 12:30:40 -0400

    updated gaming logic 5.20

--- js/main.js
+++ js/main.js
@@ -2,19 +2,23 @@ console.log("Up and running!");
 
 var cards = ["king", "king", "queen", "queen"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User Flipped king");
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User Flipped queen");
-var cardThree = cards[2];
-var cardFour = cards[3]
-	if (cardsInPlay.length === 2) {
-		if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You Found A Match!");
-		} else {
-			alert("Sorry! Try Again!");
-		}
-	} 
 
+var checkForMatch = function() {
+	 if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+  } else {
+      console.log("Sorry, try again.");
+  }
+}
+
+var flipCard = function (cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+};
+
+
+flipCard(0);
+flipCard(2);
